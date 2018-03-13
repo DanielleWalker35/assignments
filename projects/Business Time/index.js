53f05ae0ac61423f2cf16e8517f0092c69abf4bb
DanielleWalker35 <yeller22@gmail.com>
2018-03-12 20:59:31 -0600

    added js functions to Business Time

---
+++ projects/Business Time/index.js
@@ -0,0 +1,23 @@
+var facebook = document.getElementById("facebook");
+
+facebook.addEventListener("click", goTo);
+
+function goTo () {
+    window.location = "http://www.facebook.com";
+}
+
+var facebook = document.getElementById("instagram");
+
+facebook.addEventListener("click", goTo2);
+
+function goTo2 () {
+    window.location = "http://www.instagram.com";
+}
+
+var facebook = document.getElementById("twitter");
+
+facebook.addEventListener("click", goTo3);
+
+function goTo3 () {
+    window.location = "http://www.twitter.com";
+}
